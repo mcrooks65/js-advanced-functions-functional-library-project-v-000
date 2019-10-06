695ad4cf836b4d1caab28220cda31d1f255a077d
Matthew Crooks <mfcrooks@gmail.com>
2019-10-06 23:42:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iterater) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for (let idx = 0; idx < newCollection.length; idx++)
+        iteratee(newCollection[idx])
+
+      return collection
     },
 
     map: function() {
